6776d185ee1f2bdb40f8af85f0bb8d70b191498d
Vivek Tiwari <vivek.tiwari@velocis.in>
2024-05-31 17:32:11 +0530

    We are to some chenges for App.js to add new commpponent

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import About from './page/About';
+import Contect from './page/Contect';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
       </header>
     </div>
     <About/>
+    <Contect/>
     </>
   );
 }
